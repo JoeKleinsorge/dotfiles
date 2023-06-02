e1c32d74a440767d9ef022f8214bbaa658923e80
Joe Kleinsorge <joe@kleinsorge.dev>
2023-06-02 09:11:28 -0400

    chore: update brewfile

--- terminal/link.sh
+++ terminal/link.sh
@@ -2,9 +2,13 @@
 
 # Symlink dotfiles
 ## ZSH
-ln -s ~/git/dotfiles/base/zsh/.zshrc ~/.zshrc
-ln -s ~/git/dotfiles/base/kitty/.zshenv ~/.zshenv
+ln -s ~/git/dotfiles/terminal/zsh/.zshrc ~/.zshrc
 
 ## Kitty
-ln -s ~/git/dotfiles/base/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/git/dotfiles/base/kitty/kitty-themes.conf ~/.config/kitty/kitty-themes.conf
+ln -s ~/git/dotfiles/terminal/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/git/dotfiles/terminal/kitty/kitty-themes.conf ~/.config/kitty/kitty-themes.conf
+cp ~/git/dotfiles/terminal/kitty/kitty.app.png ~/.config/kitty/kitty.app.png
+rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
+
+## LunarVim
+ln -s ~/git/dotfiles/terminal/lunarvim/config.lua  ~/.config/lvim/config.lua
