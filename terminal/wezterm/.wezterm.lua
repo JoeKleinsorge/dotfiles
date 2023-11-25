44a750f44a3e91473e596f867bbea52c1b1690ce
Joe Kleinsorge <joe@kleinsorge.dev>
2023-11-24 21:27:26 -0500

    ci: commit

--- terminal/wezterm/.wezterm.lua
+++ terminal/wezterm/.wezterm.lua
@@ -1,15 +1,13 @@
 local wezterm = require 'wezterm'
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+ config = wezterm.config_builder()
 end
 
--- config goes here
-config.window_background_opacity = 0.9
-config.color_scheme = 'Pnevma'
--- config.color_scheme = 'Everforest Dark (Gogh)'
-config.font =
-    wezterm.font('JetBrains Mono', { weight = 'Bold' })
-config.font_size = 15.0
-return config
+ config.window_background_opacity = 0.94
+ config.color_scheme = 'Pnevma'
+ config.font =
+     wezterm.font('JetBrains Mono', { weight = 'Bold' })
+ config.font_size = 15.0
+ return config
 
