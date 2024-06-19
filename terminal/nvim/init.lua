2991b8afe13db421fd0d7b3d5f69be771e87e37a
Joe Kleinsorge <joe@kleinsorge.dev>
2024-06-19 15:20:40 -0400

    ci: commit

--- terminal/nvim/init.lua
+++ terminal/nvim/init.lua
@@ -1,34 +1,16 @@
-require("user.launch")
-require("user.options")
-require("user.keymaps")
-require("user.autocmds")
-spec("user.colorscheme")
-spec("user.plenary")
--- spec("user.hardtime")
-spec("user.gen")
-spec("user.undotree")
-spec("user.todo-comments")
-spec("user.flash")
-spec("user.copilot")
-spec("user.devicons")
-spec("user.treesitter")
-spec("user.mason")
-spec("user.cellular")
-spec("user.lspconfig")
-spec("user.illuminate")
-spec("user.telescope")
-spec("user.nvimtree")
-spec("user.lualine")
-spec("user.whichkey")
-spec("user.git")
-spec("user.fold")
-spec("user.autopairs")
-spec("user.comment")
-spec("user.diffview")
-spec("user.indentline")
-spec("user.alpha")
-spec("user.netrw")
-spec("user.notes")
-spec("user.project")
-spec("user.toggleterm")
-require("user.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("options")
+require("autocmds")
+require("keymaps")
+require("lazy").setup("plugins")
