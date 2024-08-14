c3fc96384340db1b987c81531df0f45d6b7c1f6c
Joe Kleinsorge <joe@kleinsorge.dev>
2024-08-14 10:17:26 -0400

    ci: commit

--- terminal/wezterm/.wezterm.lua
+++ terminal/wezterm/.wezterm.lua
@@ -3,8 +3,9 @@ local config = {}
 if wezterm.config_builder then
  config = wezterm.config_builder()
 end
-
+ config.color_scheme_dirs = { '~/git/dotfiles/terminal/wezterm/themes' }
  config.window_background_opacity = 0.94
+ -- config.color_scheme = 'Tiniri Dark'
  config.color_scheme = 'Pnevma'
  config.font =
      wezterm.font('JetBrains Mono', { weight = 'Bold' })
